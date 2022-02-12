620cd0513767a2ea57a9c3139dee95e010e9d673
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2022-02-12 13:09:53 -0300

    Crossover e mutação

--- algoritmo_genetico.py
+++ algoritmo_genetico.py
@@ -29,11 +29,40 @@ class Individuo():
             if self.cromossomo[i]=="1":
                 nota +=self.valores[i]
                 soma_espacos +=self.espacos[i]
-            if soma_espacos > self.limite_espacos: # se o tamanho dos produtos levadores forem maiores do que o limite, superou o valor da carga, entao nao eh solucao boa
+            if soma_espacos > self.limite_espacos: # se o tamanho dos produtos levados forem maiores do que o limite, superou o valor da carga, entao nao eh solucao boa
                 nota = 1
             self.nota_avaliacao = nota
             self.espaco_usado = soma_espacos
-                
+    
+    def crossover(self, outro_individuo):
+        corte = round(random() * len(self.cromossomo)) # pega numero aleatorio (entre 0 e 1) e multiplica pelo tamanho do cromossomo (tamanho do dominio)
+        
+        # ex: se o numero aleatorio for 0.3 e forem 14 produtos, logo seria 4,2, com o round seria 4, então o corte seria até o cromossomo 4
+        
+        filho1 = outro_individuo.cromossomo[0:corte] + self.cromossomo[corte::]  # :: significa que vai pegar do corte até o final;
+        # vai pegar metade (até onde for o corte) dos cromossomos do individuo2 depois a outra metade dos cromossomos do individuo1 (self)
+        filho2 = self.cromossomo[0:corte] + outro_individuo.cromossomo[corte::] # contrario do filho1
+        
+        filhos = [Individuo(self.espacos, self.valores, self.limite_espacos, self.geracao+1), 
+                  Individuo(self.espacos, self.valores, self.limite_espacos, self.geracao+1)] # cada filho vai ser um novo membro da classe individuo
+        # nova geração por isso + 1
+            
+        filhos[0].cromossomo = filho1
+        filhos[1].cromossomo = filho2
+        
+        return filhos
+    
+    def mutacao(self, taxa_mutacao):
+        print("Antes %s " % self.cromossomo)
+        for i in range(len(self.cromossomo)):
+            # se random foi maior que taxa_mutacao entao ocorre a mutacao (troca os genes)
+            if random() < taxa_mutacao:
+                if self.cromossomo[i]=='1':
+                    self.cromossomo[i]='0'
+                else:
+                    self.cromossomo[i]='1'
+        print("Depois %s " % self.cromossomo)            
+        return self
         
 if __name__ == '__main__': 
     lista_produtos = [] # inicia lista
@@ -69,3 +98,20 @@ if __name__ == '__main__':
         if individuo1.cromossomo[i] == "1": # tá na carga
             print("Nome : %s R$ %s " % (lista_produtos[i].nome, lista_produtos[i].valor))
     
+    individuo1.avaliacao()
+    print(" Nota = %s" % individuo1.nota_avaliacao)
+    print(" Espaço usado = %s" % individuo1.espaco_usado)
+    
+    # CRIA 2 INDIVIDUOS PARA FAZER O CROSSOVER
+    
+    individuo2 = Individuo(espacos, valores, limite) # cria objeto
+    
+    for i in range(len(lista_produtos)):
+        if individuo2.cromossomo[i] == "1": # tá na carga
+            print("Nome : %s R$ %s " % (lista_produtos[i].nome, lista_produtos[i].valor))
+    
+    individuo2.avaliacao()
+    print(" Nota = %s" % individuo2.nota_avaliacao)
+    print(" Espaço usado = %s" % individuo2.espaco_usado)
+    
+    individuo1.crossover(individuo2) # faz crossover dos dois individuos
