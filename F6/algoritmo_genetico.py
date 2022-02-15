78f78d3832612681f2f87e248ff90922825dba14
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2022-02-15 10:07:53 -0300

    Finalizado otimização F6 através do Algoritmo Genético

--- F6/algoritmo_genetico.py
+++ F6/algoritmo_genetico.py
@@ -4,7 +4,7 @@ import math
 class Individuo():
     def __init__(self, espacos, geracao = 0): # construtor
         self.espacos = espacos # cada produto vai ocupar um espaco
-        self.nota_avaliacao = 0
+        self.nota_final = 0
         self.geracao = geracao
         self.x = 0
         self.y = 0
@@ -29,34 +29,37 @@ class Individuo():
         F6 = 0
         
         F6 = 0.5 - ((pow(math.sin(math.sqrt(pow(self.x,2) + pow(self.y,2))),2)-0.5)/pow(1 + 0.001*(pow(self.x,2) + pow(self.y,2)),2))
-        self.nota_avaliacao = F6
+        self.nota_final = F6
            
     
-    def crossover(self, outro_individuo):
-        corte = round(random() * len(self.cromossomo)) # pega numero aleatorio (entre 0 e 1) e multiplica pelo tamanho do cromossomo (tamanho do dominio)
+    def crossover(self, outro_individuo, taxa_crossover):
+        particiona = round(random() * len(self.cromossomo)) # pega numero aleatorio (entre 0 e 1) e multiplica pelo tamanho do cromossomo (tamanho do dominio)
         
     
         # ex: se o numero aleatorio for 0.3 e forem 14 produtos, logo seria 4,2, com o round seria 4, então o corte seria até o cromossomo 4
         
-        filho1 = outro_individuo.cromossomo[0:corte] + self.cromossomo[corte::]  # :: significa que vai pegar do corte até o final;
+        filho1 = outro_individuo.cromossomo[0:particiona] + self.cromossomo[particiona::]  # :: significa que vai pegar do corte até o final;
         # vai pegar metade (até onde for o corte) dos cromossomos do individuo2 depois a outra metade dos cromossomos do individuo1 (self)
-        filho2 = self.cromossomo[0:corte] + outro_individuo.cromossomo[corte::] # contrario do filho1
+        filho2 = self.cromossomo[0:particiona] + outro_individuo.cromossomo[particiona::] # contrario do filho1
         
         filhos = [Individuo(self.espacos,  self.geracao+1), 
                   Individuo(self.espacos,  self.geracao+1)] # cada filho vai ser um novo membro da classe individuo
         # nova geração por isso + 1
-            
-        filhos[0].cromossomo = filho1
-        filhos[1].cromossomo = filho2
+        if random() < taxa_crossover:  
+            filhos[0].cromossomo = filho1
+            filhos[1].cromossomo = filho2
+        else:
+            filhos[0].cromossomo = self.cromossomo
+            filhos[1].cromossomo = outro_individuo.cromossomo
         
         return filhos
     
     # há uma possibilidade, mesmo que pequena, de mutar os genes
-    def mutacao(self, taxa_mutacao):
+    def mutacao(self, tx_mutacao):
         
         for i in range(len(self.cromossomoLista)):
-            # se random foi maior que taxa_mutacao entao ocorre a mutacao (troca os genes)
-            if random() < taxa_mutacao:
+            # se random foi maior que tx_mutacao entao ocorre a mutacao (troca os genes)
+            if random() < tx_mutacao:
                 if self.cromossomoLista[i]=='1':
                     self.cromossomoLista[i]='0'
                 else:
@@ -74,140 +77,143 @@ class Individuo():
         return decimal
     
     
-class AlgoritmoGenetico(): # vai armazenar objetos do tipo Individuo
-    def __init__(self,tamanho_populacao): # quantos individuos vou criar
-        self.tamanho_populacao = tamanho_populacao
+class AG(): # vai armazenar objetos do tipo Individuo
+    def __init__(self,pop_size): # quantos individuos vou criar
+        self.pop_size = pop_size
         self.populacao = [] # vetor de individuos
         self.geracao = 0
-        self.melhor_solucao = 0 # qual dos individuos terá a melhor solucao (maior nota) e que nao extrapole o limite
+        self.mais_otimizado = 0 # qual dos individuos terá a melhor solucao (maior nota) e que nao extrapole o limite
         
     def inicializa_populacao(self, espacos):
-        for i in range(self.tamanho_populacao):
+        for i in range(self.pop_size):
             self.populacao.append(Individuo(espacos))
         
-        self.melhor_solucao=self.populacao[0] # de primeiro palpite, seta o primeiro individuo como melhor solucao
+        self.mais_otimizado=self.populacao[0] # de primeiro palpite, seta o primeiro individuo como melhor solucao
         
-      
-    def ordena_populacao(self):
-        # ordena baseado na nota_avaliacao
-        self.populacao = sorted(self.populacao, key = lambda populacao: populacao.nota_avaliacao,
-                                reverse= True)
          
     # GUARDA O INDIVIDUO COM MELHOR SOLUÇÃO DENTRE TODAS AS GERAÇÕES
-    def melhor_individuo(self, individuo): # checa se a nota do individuo recebido é melhor que a nota da melhor solução
-        if individuo.nota_avaliacao > self.melhor_solucao.nota_avaliacao: # melhor_solucao carrega o objeto do individuo
-            self.melhor_solucao = individuo # se for, o individuo da melhor solucao muda
+    def individuo_destaque(self, individuo): # checa se a nota do individuo recebido é melhor que a nota da melhor solução
+        if individuo.nota_final > self.mais_otimizado.nota_final: # mais_otimizado carrega o objeto do individuo
+            self.mais_otimizado = individuo # se for, o individuo da melhor solucao muda
        
-    def soma_avaliacoes(self): # somatorio de todas avaliações dos individuos (que cromossomo==1)
+    def ordenar(self):
+        # ordena baseado na nota_final
+        self.populacao = sorted(self.populacao, key = lambda populacao: populacao.nota_final,
+                                reverse= True)
+    def soma_notas(self): # somatorio de todas avaliações dos individuos (que cromossomo==1)
         soma=0
         for individuo in self.populacao:
-            soma += individuo.nota_avaliacao # nota_avaliacao tem todas as somas dos valores
+            soma += individuo.nota_final # nota_final tem todas as somas dos valores
         
         return soma
     # INDIVIDUOS MAIS APTOS = AVALIAÇÃO MAIOR. Quem tem maior avaliação, tem mais chance de ser selecionado
     
     # roleta viciada, retorna id do individuo selecionado
-    def seleciona_pai(self, soma_avaliacao): # sorteia o pai baseado no valor_sorteado (roleta)
+    def roleta(self, soma_notas): # sorteia o pai baseado no valor_sorteado (roleta)
         pai = -1 # nao selecionou nenhum individuo
-        valor_sorteado = random() * soma_avaliacao
-        soma=0
-        i =0
-        while i<len(self.populacao) and soma < valor_sorteado:
-            soma += self.populacao[i].nota_avaliacao
+        valor_aleatorio = random() * soma_notas
+        soma, i =0, 0
+       
+        while i<len(self.populacao) and soma < valor_aleatorio:
+            soma += self.populacao[i].nota_final
             pai+=1
             i += 1
-        # ex: se a soma_avaliacao for 50000 e for multiplicado por 0.4, será 20000.
-        # depois ele pegará a nota_avaliacao de cada individuo nessa populacao e somar
+        # ex: se a soma_notas for 50000 e for multiplicado por 0.4, será 20000.
+        # depois ele pegará a nota_final de cada individuo nessa populacao e somar
         # quando o valor de soma for maior que o valor sorteado, será escolhido o pai (para o crossover)
         return pai # retorna indice do pai selecionado
 
-    def visualiza_geracao(self):# qual espaço e qual cromossomo utilizado
+    def cromossomo_otimizado(self):# qual espaço e qual cromossomo utilizado
         melhor = self.populacao[0] # vetor ordenado, logo 0 será o melhor
         #print("G:%s -> Cromossomo: %s" % (self.populacao[0].geracao,
               # melhor.cromossomo))
     
     
-    def resolver(self, taxa_mutacao, numero_geracoes, espacos, minimo, maximo):
+    def otimizar(self, tx_mutacao, num_ger, espacos, minimo, maximo, total_indiv, taxa_crossover):
         # POPULAÇÃO INICIAL
         self.inicializa_populacao(espacos) # geração da população
-        
+       
         # converte binário pra decimal
         for pop in self.populacao:
-            
+            # conversão de base
             pop.x = pop.converte_base(int(pop.x))
             pop.y = pop.converte_base(int(pop.y))
             
-            pop.x = minimo + round(pop.x * (self.tamanho_populacao/(pow(2,espacos/2)-1)), 5)
-            pop.y = maximo + round(pop.y * (self.tamanho_populacao/(pow(2,espacos/2)-1)), 5)
+            # decodificação
+            pop.x = minimo + round(pop.x * ((maximo-minimo)/(pow(2,espacos/2)-1)), 4)
+            pop.y = minimo + round(pop.y * ((maximo-minimo)/(pow(2,espacos/2)-1)), 4)
             
             pop.avaliacao()
         
-        self.ordena_populacao()
+        self.ordenar()
       
         # for i in range(self.tamanho_populacao) :
         #      print("*** INDIVIDUO %s ***\n" % i,
-        #       "Espaços = %s\n" % str(ag.populacao[i].espacos),
-        #       "X = %s\n" % str(ag.populacao[i].x),
-        #       "Y = %s\n" % str(ag.populacao[i].y),
-        #        "Cromossomo = %s\n" % str(ag.populacao[i].cromossomo),
-        #       "Nota = %s\n" % ag.populacao[i].nota_avaliacao)
+        #       "Espaços = %s\n" % str(algoritmo.populacao[i].espacos),
+        #       "X = %s\n" % str(algoritmo.populacao[i].x),
+        #       "Y = %s\n" % str(algoritmo.populacao[i].y),
+        #        "Cromossomo = %s\n" % str(algoritmo.populacao[i].cromossomo),
+        #       "Nota = %s\n" % algoritmo.populacao[i].nota_final)
             
-        self.visualiza_geracao() # pega o melhor individuo de cada populacao(geração) 
+        self.cromossomo_otimizado() # pega o melhor individuo de cada populacao(geração) 
         # (escolhe o melhor individuo entre 20 individuos (tamanho_populacao) dessa geração)
         
         # seleciona pais, crossover, mutaçao
-        # vai fazer a comparação do melhor_individuo da população inicial com o
+        # vai fazer a comparação do individuo_destaque da população inicial com o
         # de todas as outras gerações
         
-        for geracao in range(numero_geracoes):
-            soma_avaliacao = self.soma_avaliacoes()
+        for geracao in range(num_ger):
+            soma_notas = self.soma_notas()
             nova_populacao = []
             
-            for individuos_gerados in range(0, self.tamanho_populacao, 2):
-                pai1= self.seleciona_pai(soma_avaliacao)
-                pai2= self.seleciona_pai(soma_avaliacao)
+            for individuos_gerados in range(0, self.pop_size, 2):
+                pai_1= self.roleta(soma_notas)
+                pai_2= self.roleta(soma_notas)
                 
-                filhos = self.populacao[pai1].crossover(self.populacao[pai2])
+               
                 
-                nova_populacao.append(filhos[0].mutacao(taxa_mutacao))
-                nova_populacao.append(filhos[1].mutacao(taxa_mutacao))
+                filhos = self.populacao[pai_1].crossover(self.populacao[pai_2], taxa_crossover)
                 
-            self.populacao = list(nova_populacao) #sobreescrevi população antiga
+                    
+                nova_populacao.append(filhos[0].mutacao(tx_mutacao))
+                nova_populacao.append(filhos[1].mutacao(tx_mutacao))
+                
+            self.populacao = list(nova_populacao) # sobreescrevi população antiga
             
             for pop in self.populacao:
                 
                 pop.x = pop.converte_base(int(pop.x))
                 pop.y = pop.converte_base(int(pop.y))
                 
-                pop.x = minimo + round(pop.x * (self.tamanho_populacao/(pow(2,espacos/2)-1)), 5)
-                pop.y = maximo + round(pop.y * (self.tamanho_populacao/(pow(2,espacos/2)-1)), 5)
+                pop.x = minimo + round(pop.x * ((maximo-minimo)/(pow(2,espacos/2)-1)), 4)
+                pop.y = minimo + round(pop.y * ((maximo-minimo)/(pow(2,espacos/2)-1)), 4)
                 
                 pop.avaliacao()
             
-            self.ordena_populacao()
-            self.visualiza_geracao()
+            self.ordenar()
+            self.cromossomo_otimizado()
             
             melhor = self.populacao[0]
-            self.melhor_individuo(melhor)
+            self.individuo_destaque(melhor)
         
-        print("\nMelhor solução: G -> %s Valor: %s Cromossomo: %s" %
-              (self.melhor_solucao.geracao, self.melhor_solucao.nota_avaliacao,
-               self.melhor_solucao.cromossomo))
         
-        return self.melhor_solucao.cromossomo # vai retornar o cromossomo da melhor solução
+        return self.mais_otimizado # vai retornar o cromossomo da melhor solução
         
 if __name__ == '__main__': 
    
-    espacos = 44 # guarda todos os espaços dos produtos
+    espacos = 44 # bits
     minimo = -100
     maximo = 100
-    tamanho_populacao = maximo - minimo  # cria 20 individuos com os mesmos espaços e valores, mas cromossomos e notas diferentes
-    taxa_mutacao = 0.01
-    numero_geracoes = 40
-    
-    ag = AlgoritmoGenetico(tamanho_populacao)
-    
-    
-    resultado = ag.resolver(taxa_mutacao, numero_geracoes, espacos, minimo, maximo)
+    pop_size = 100
+    tx_mutacao = 0.01
+    num_ger = 40
+    taxa_crossover = 0.65
+    total_ind = pop_size * num_ger
+    algoritmo = AG(pop_size)
     
     
+    final = algoritmo.otimizar(tx_mutacao, num_ger, espacos, minimo, maximo, total_ind, taxa_crossover)
+    
+    print("\nMelhor otimização encontrada: Geração -> %s Valor: %s Cromossomo: %s X: %s Y: %s" %
+          (final.geracao, final.nota_final,
+           final.cromossomo, final.x, final.y))
